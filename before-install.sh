499e713e7bc5a6d6b4957eb9a8a8a995740b4b3a
Andrew Leonard <andy@hurricane-ridge.com>
2012-12-02 06:53:11 -0800

    Initial implementation of before-install.sh script.

---
+++ before-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+getent group bind >/dev/null 2>&1 || addgroup --system bind
+getent passwd bind >/dev/null 2>&1 ||
+    adduser --system --home /var/cache/bind --no-create-home \
+           --disabled-password --ingroup bind bind
+
+# We're assuming a chroot environment, so no populating/permissioning
+# directories here.  It might look something like this (from Ubuntu postinst
+# script):
+#
+#       chgrp bind /etc/bind
+#       chmod g+s /etc/bind
+#       chgrp bind /etc/bind/rndc.key /var/cache/bind
+#       chgrp bind /etc/bind/named.conf* || true
+#       chmod g+r /etc/bind/rndc.key /etc/bind/named.conf* || true
+#       chmod g+rwx /var/cache/bind
